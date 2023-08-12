391dfac592efe6cd88981272c2970f5b74d83d19
Juraj Baňas <juraj.banas@student.tuke.sk>
2023-08-12 08:52:48 +0200

    Hello World in c++

---
+++ cpp/hello_world/hello_world.cpp
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+#include <iostream>
+
+void HelloWorld::greet() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ cpp/hello_world/main.cpp
@@ -0,0 +1,8 @@
+#include "hello_world.h"
+
+int main() {
+    HelloWorld myHelloWorld;
+    myHelloWorld.greet();
+
+    return 0;
+}
